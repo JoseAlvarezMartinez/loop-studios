be936ae2f69e64022f406e01d411aeb9790a46b0
Jose Alvarez <alvarezmjose2021@gmail.com>
2024-04-15 11:33:50 -0300

    -- Tablet Presentation --

--- src/layout/Presentation/Presentation.jsx
+++ src/layout/Presentation/Presentation.jsx
@@ -6,10 +6,12 @@ const Presentation = () => {
         <section className="presentation-container">
             <img className="presentation-image" src={PresentationImage} alt="Imagen del main" />
 
-            <h2 className="presentation-h2">The leader in interactive vr</h2>
-            <p className="presentation-p">Founded in 2011, Loopstudios has been producing world-class virtual reality
-                projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.
-            </p>
+            <div className="presentation-text-container">
+                <h2 className="presentation-h2">The leader in interactive vr</h2>
+                <p className="presentation-p">Founded in 2011, Loopstudios has been producing world-class virtual reality
+                    projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.
+                </p>
+            </div>
         </section>
     )
 }
